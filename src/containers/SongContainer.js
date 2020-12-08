bed3c8591de5b30a62baa339948792ef1622d069
AvianLearning <iaradcliffe@hotmail.com>
2020-12-08 16:33:24 +0000

    Added fetch method in SongContainer

--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
 import SongList from '../components/SongList'
 
 const SongContainer = () => {
+
+    const [songs, setSongs] = useState({});
+
+    const getSongs = () => {
+        console.log("Getting song data");
+        fetch('https://itunes.apple.com/gb/rss/topsongs/limit=20/json')
+        .then(res => res.json())
+        .then(data => setSongs(data))
+    }
+
+    useEffect(() => {
+        getSongs();
+    }, []);
 
     return (
         <>
